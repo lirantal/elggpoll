61a39f2642c6d2f77711f4943e5d2eac954453fb
Liran Tal <liran.tal@gmail.com>
2009-12-11 17:28:36 +0000

    fixed deletion of poll to redirect to group if this is a group poll

--- actions/delete.php
+++ actions/delete.php
@@ -7,31 +7,35 @@
  * Code modified by 
  * Team Webgalli, Vinsoft di Erminia Naccarato, www.vinsoft.it
  */
-	 
+
 
 	// Make sure we're logged in (send us to the front page if not)
-		gatekeeper();
-
+	gatekeeper();
+	
 	// Get input data
-		$guid = (int) get_input('pollpost');
-		
+	$guid = (int) get_input('pollpost');
+	
 	// Make sure we actually have permission to edit
-		$poll = get_entity($guid);
-		if ($poll->getSubtype() == "poll" && $poll->canEdit()) {
+	$poll = get_entity($guid);
+	if ($poll->getSubtype() == "poll" && $poll->canEdit()) {
 	
 		// Get owning user
-				$owner = get_entity($poll->getOwner());
+		$owner = get_entity($poll->getOwner());
+		$containerEntity = get_entity($poll->container_guid);
+		if ($containerEntity instanceof ElggGroup)
+			$owner = $containerEntity;
+		
 		// Delete it!
-				$rowsaffected = $poll->delete();
-				if ($rowsaffected > 0) {
-		// Success message
-					system_message(elgg_echo("poll:deleted"));
-				} else {
-					register_error(elgg_echo("poll:notdeleted"));
-				}
-		// Forward to the main poll page
-				forward("pg/poll/".$owner->username);
-		
+		$rowsaffected = $poll->delete();
+		if ($rowsaffected > 0) {
+			// Success message
+			system_message(elgg_echo("poll:deleted"));
+		} else {
+			register_error(elgg_echo("poll:notdeleted"));
 		}
-		
+		// Forward to the main poll page
+		forward("pg/poll/".$owner->username);
+	
+	}
+
 ?>
