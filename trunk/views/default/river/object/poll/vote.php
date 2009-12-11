b4715bda422985354d8c53f6f51c472f0c8e3c1a
liran.tal <liran.tal@2b00f262-d8da-11de-9872-ef835a306e00>
2009-12-11 15:17:56 +0000

    updated river notifications to reflect group relationship if exists

--- trunk/views/default/river/object/poll/vote.php
+++ trunk/views/default/river/object/poll/vote.php
@@ -1,12 +1,27 @@
 <?php
+/**
+ * Elgg Poll plugin
+ * @package poll
+ * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
+ * @author	Liran Tal
+ * Code modified by 
+ * Team Webgalli, Vinsoft di Erminia Naccarato, www.vinsoft.it
+ */
 
 	$performed_by = get_entity($vars['item']->subject_guid); // $statement->getSubject();
 	$object = get_entity($vars['item']->object_guid);
-	$url = $object->getURL();
+	$containerEntity = get_entity($object->container_guid);
+	$poll_group_info = "";
+	if ($containerEntity instanceof ElggGroup) {
+		$poll_url = $CONFIG->wwwroot."pg/poll/".$containerEntity->username."/read/".$object->guid;
+		$poll_group_info = " " . elgg_echo('groups:ingroup') . " <a href='".$containerEntity->getURL()."' >" . $containerEntity->name  . "</a>";
+	} else
+		$poll_url = $object->getURL();
 	
 	$url = "<a href=\"{$performed_by->getURL()}\">{$performed_by->name}</a>";
 	$string = sprintf(elgg_echo("poll:river:voted"),$url) . " ";
-	$string .= elgg_echo("poll:river:vote") . " <a href=\"" . $object->getURL() . "\">" . $object->question . "</a>";
+	$string .= elgg_echo("poll:river:vote") . " <a href='$poll_url' >" . $object->question . "</a>";
+	$string .= $poll_group_info;
 	
 ?>
 
